e385c792da15684391f8cbcf4414f66b1e38afde
AVIX <avix256@gmail.com>
2020-09-15 19:22:52 +0700

    small changes

--- src/main.c
+++ src/main.c
@@ -2,29 +2,35 @@
 #include "Window.h"
 #include "Vector2.h"
 
-static const GREEN[3] = {0, 255, 0};
+static const int GREEN[3] = {0, 255, 0};
 
 static void Loop(void *window)
 {
-    Vector2 prev;
-    
-    for (int i = 1; 1; i++)
+    while (1)
     {
-        printf("%d: ", i);
-        Vector2 cur = Vector2Read();
-        Display(cur);
-        DrawVector2(window, cur);
-        if (i > 1)
-        {
-            printf("Angle between %d and %d: %f\n", i - 1, i, Angle(cur, prev));
-            
-            Vector2 sum = Add(cur, prev);
-            printf("Vector %d + %d = ", i - 1, i);
-            Display(sum);
-            sum.color = GREEN;
-            DrawVector2(window, sum);
-        }
-        prev = cur;
+        WindowClear(window);
+
+        Vector2 v1 = Vector2Read();
+        printf("v1 = ");
+        Display(v1);
+        DrawVector2(window, v1);
+
+        Vector2 v2 = Vector2Read();
+        printf("v2 = ");
+        Display(v2);
+        DrawVector2(window, v2);
+
+        printf("Angle between v1 and v2: %f\n", Angle(v1, v2));
+
+        Vector2 sum = Add(v1, v2);
+        printf("v1 + v2 = ");
+        Display(sum);
+        sum.color = GREEN;
+        DrawVector2(window, sum);
+
+        printf("\n");
+        system("pause");
+        system("cls");
     }
 }
 
@@ -32,7 +38,6 @@ int main(int argc, char *argv[])
 {
     SDL_Window *window = WindowInit();
     SDL_Thread *thread = SDL_CreateThread(Loop, "TestThread", (void *)window);
-    WindowClear(window);
 
     SDL_Event event;
     while (SDL_WaitEvent(&event) >= 0)
